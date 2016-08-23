9a71ba6fe5a3ed064dac9947c66cbdf970daf434
Marcus Haase <mail@marcus.haase.name>
2016-08-23 09:33:58 +0200

    initial import

---
+++ src/Textcat.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace HaaseIT\HCSFNG\Frontend;
+
+
+class Textcat
+{
+    protected $T, $sLang, $sDefaultlang, $bVerbose, $logdir;
+
+    public function __construct($container, $defaultlang, $verbose = false, $logdir = '')
+    {
+        $this->sLang = \filter_var($container['lang'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+        $this->sDefaultlang = \filter_var($defaultlang, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+        $this->bVerbose = $verbose;
+        $this->logdir = $logdir;
+    }
+
+    public function loadTextcats()
+    {
+        // get textcat for the current language
+        if (is_file(PATH_TEXTCATS.$this->sLang.'.json')) {
+            $aTextcat[$this->sLang] = json_decode(file_get_contents(PATH_TEXTCATS.$this->sLang.'.json'));
+        }
+
+        // if the current language is not the default language, get the defauld language aswell
+        if ($this->sLang != $this->sDefaultlang) {
+            if (is_file(PATH_TEXTCATS.$this->sDefaultlang.'.json')) {
+                $aTextcat[$this->sDefaultlang] = json_decode(file_get_contents(PATH_TEXTCATS.$this->sDefaultlang.'.json'));
+            }
+        }
+
+        if (isset($aTextcat)) {
+            $this->T = $aTextcat;
+        }
+    }
+
+    /**
+     * @param $sTextkey
+     * @param bool $bReturnFalseIfNotAvailable
+     * @return bool|string
+     */
+    public function T($sTextkey, $bReturnFalseIfNotAvailable = false)
+    {
+        $return = '';
+        $sTextkey = \filter_var($sTextkey, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+        if (isset($_GET["showtextkeys"])) {
+            $return = '['.$sTextkey.']';
+        } else {
+            if (!empty($this->T[$this->sLang][$sTextkey]["tcl_text"]) && \trim($this->T[$this->sLang][$sTextkey]["tcl_text"]) != '') {
+                $return = \trim($this->T[$this->sLang][$sTextkey]["tcl_text"]);
+            } elseif (!empty($this->T[$this->sDefaultlang][$sTextkey]["tcl_text"]) && \trim($this->T[$this->sDefaultlang][$sTextkey]["tcl_text"]) != '') {
+                $return = \trim($this->T[$this->sDefaultlang][$sTextkey]["tcl_text"]);
+            }
+            if (!isset($return) || $return == '') {
+                if ($this->logdir != '' && is_dir($this->logdir) && is_writable($this->logdir)) {
+                    error_log(date('c').' Missing Text: '.$sTextkey.PHP_EOL, 3, $this->logdir.DIRECTORY_SEPARATOR.'errors_textcats.log');
+                }
+                if ($bReturnFalseIfNotAvailable) return false;
+                elseif ($this->bVerbose) $return = 'Missing Text: '.$sTextkey;
+            }
+        }
+
+        return $return;
+    }
+}
