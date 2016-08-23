97e06d60362a5d933d0b4a33ddaa51556be0de40
Marcus Haase <mail@marcus.haase.name>
2016-08-23 09:37:04 +0200

    initial import

---
+++ src/Helper.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace HaaseIT\HCSFNG\Frontend;
+
+
+class Helper
+{
+    public static function getLanguage($container)
+    {
+        $langavailable = $container['conf']["lang_available"];
+        if (
+            $container['conf']["lang_detection_method"] == 'domain'
+            && isset($container['conf']["lang_by_domain"])
+            && is_array($container['conf']["lang_by_domain"])
+        ) { // domain based language detection
+            foreach ($container['conf']["lang_by_domain"] as $sKey => $sValue) {
+                if ($_SERVER["SERVER_NAME"] == $sValue || $_SERVER["SERVER_NAME"] == 'www.'.$sValue) {
+                    $sLang = $sKey;
+                    break;
+                }
+            }
+        } elseif ($container['conf']["lang_detection_method"] == 'legacy') { // legacy language detection
+            $sLang = key($langavailable);
+            if (isset($_GET["language"]) && array_key_exists($_GET["language"], $langavailable)) {
+                $sLang = strtolower($_GET["language"]);
+                setcookie('language', strtolower($_GET["language"]), 0, '/');
+            } elseif (isset($_COOKIE["language"]) && array_key_exists($_COOKIE["language"], $langavailable)) {
+                $sLang = strtolower($_COOKIE["language"]);
+            } elseif (isset($_SERVER["HTTP_ACCEPT_LANGUAGE"]) && array_key_exists(substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2), $langavailable)) {
+                $sLang = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
+            }
+        }
+        if (!isset($sLang)) {
+            $sLang = key($langavailable);
+        }
+
+        return $sLang;
+    }
+}
